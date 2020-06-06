61924afa15bde553b1e2f8775eae206e3af91df7
ymizushi <mizushi@gmail.com>
2020-06-07 01:37:36 +0900

    fix code

--- src/emola/eval.rs
+++ src/emola/eval.rs
@@ -1,5 +1,22 @@
-use super::parse::Tree;
 use std::cmp::PartialEq;
+use std::collections::HashMap;
+
+use super::parse::Tree;
+
+#[derive(PartialEq, Debug)]
+pub struct Env<'a> {
+    pub map: HashMap<String, Value<'a>>,
+}
+
+impl<'a> Env<'a> {
+    fn find(&self, key: String) -> Option<&Value> {
+        self.map.get(&key)
+    }
+
+    fn insert(&mut self, key: String, v: Value<'a>) -> Value<'a> {
+        self.map.insert(key, v).unwrap()
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Value<'a> {
@@ -9,8 +26,7 @@ pub enum Value<'a> {
     Int(i32)
 }
 
-
-pub fn eval<'a>(t: Tree<&'a str>, env: &Env) -> Value<'a> {
+pub fn eval<'a>(t: &Tree<&'a str>, env: &RefCell<Env<'a>>) -> Value<'a> {
     use Tree::*;
     match t {
         Leaf(l) => {
@@ -21,6 +37,12 @@ pub fn eval<'a>(t: Tree<&'a str>, env: &Env) -> Value<'a> {
                 Leaf("+") => {
                     adder(v[1..].to_vec(), env)
                 },
+                Leaf("fn") => {
+                    function(v[1..].to_vec(), env)
+                },
+                Leaf("def") => {
+                    define(v[1..].to_vec(), env)
+                },
                 _ => {
                     Value::Int(32)
                 }
@@ -29,56 +51,6 @@ pub fn eval<'a>(t: Tree<&'a str>, env: &Env) -> Value<'a> {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_eval() {
-        use super::*;
-        use super::super::parse::parse;
-        assert_eq!(
-            Value::Int(7),
-            eval(
-                parse(&mut vec!["(", "+", "2", "5", ")"].iter().peekable()),
-                &Env {
-                    map: HashMap::new()
-                }
-            )
-        );
-
-    }
-
-}
-
-
-use std::collections::HashMap;
-
-#[derive(PartialEq)]
-pub struct Env<'a> {
-    pub map: HashMap<String, Value<'a>>,
-}
-
-use std::fmt;
-impl<'a> std::fmt::Debug for Env<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-       f.debug_struct("Env")
-         .field("map", &"map")
-         .finish()
-    }
-}
-
-impl<'a> Env<'a> {
-    fn find(&self, key: String) -> Option<&Value> {
-        self.map.get(&key)
-    }
-
-    fn insert(&mut self, key: String, v: Value<'a>) {
-        self.map.insert(key, v);
-    }
-}
-
-
-
 
 fn to_value(l: &str) -> Value {
     if l.starts_with("\"") {
@@ -91,9 +63,11 @@ fn to_value(l: &str) -> Value {
     }
 }
 
-fn adder<'a>(v: Vec<Tree<&'a str>>, env: &Env) -> Value<'a> {
+use std::cell::RefCell;
+
+fn adder<'a>(v: Vec<Tree<&'a str>>, ev: &RefCell<Env<'a>>) -> Value<'a> {
     v.iter()
-        .map(|x| eval(x.clone(), &env))
+        .map(|x| eval(x, ev))
         .map(|x| {
             match x {
                 Value::Int(i) => i,
@@ -106,8 +80,52 @@ fn adder<'a>(v: Vec<Tree<&'a str>>, env: &Env) -> Value<'a> {
                 _ => panic!("")
             }
         })
+}
+
+fn function<'a>(v: Vec<Tree<&'a str>>, env: &RefCell<Env<'a>>) -> Value<'a> {
+    v.iter()
+        .map(|x| eval(x, env))
+        .map(|x| {
+            match x {
+                Value::Int(i) => i,
+                _ => panic!("")
+            }
+        })
+        .fold(Value::Int(0), |acc, x| {
+            match acc {
+                Value::Int(i) => Value::Int(i+x),
+                _ => panic!("")
+            }
+        })
+}
+
+fn define<'a>(v: Vec<Tree<&'a str>>, ev: &RefCell<Env<'a>>) -> Value<'a> {
+    match (v[0].clone(), v[1].clone()) {
+        (Tree::Leaf(bind_key), tree) =>  {
+            let value = eval(&tree, ev);
+            ev.borrow_mut().insert(String::from(bind_key), value)
+        }
+        _ => panic!("invalid syntax of define")
+    }
+}
 
 
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_eval() {
+        use super::*;
+        use super::super::parse::parse;
+        assert_eq!(
+            Value::Int(7),
+            eval(
+                &parse(&mut vec!["(", "+", "2", "5", ")"].iter().peekable()),
+                &RefCell::new(Env {
+                    map: HashMap::new()
+                })
+            )
+        );
+    }
 }
 
 
