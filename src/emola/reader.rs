4f29044a74cb26b504f800db91f1803be980b945
ymizushi <mizushi@gmail.com>
2020-06-08 22:21:17 +0900

    fix code

--- src/emola/reader.rs
+++ src/emola/reader.rs
@@ -3,10 +3,10 @@ use super::parse::{parse, Tree};
 
 use std::io;
 
-fn read<'a>() -> Tree<String> {
+fn read<'a>() {
     let mut buff = String::new();
     io::stdin().read_line(&mut buff).unwrap();
     let tokenized: Vec<String> = tokenize(&buff);
     let mut tokens = tokenized.iter().peekable();
-    parse(&mut tokens)
+   // parse(&mut tokens)
 }
