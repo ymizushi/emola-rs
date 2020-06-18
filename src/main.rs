83755ad443030ec5c7bde9a8d8ca2c4763d17148
ymizushi <mizushi@gmail.com>
2020-06-19 08:55:15 +0900

    add basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,112 @@
 extern crate emola_rs;
 
+use std::collections::HashMap;
+use std::fmt;
+use std::io;
+use std::num::ParseFloatError;
+use std::rc::Rc;
+
+
+
+#[derive(Clone)]
+enum RispExp {
+    Bool(bool),
+    Symbol(String),
+    Number(f64),
+    List(Vec<RispExp>),
+    Func(fn(&[RispExp]) -> Result<RispExp, RispErr>),
+    Lambda(RispLambda)
+}
+
+#[derive(Clone)]
+struct RispLambda {
+    params_exp: Rc<RispExp>,
+    body_exp: Rc<RispExp>,
+}
+
+impl fmt::Display for RispExp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match self {
+            RispExp::Bool(a) => a.to_string(),
+            RispExp::Symbol(s) => s.clone(),
+            RispExp::Number(n) => n.to_string(),
+            RispExp::List(list) => {
+                let xs: Vec<String> = list
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect();
+                format!("({})", xs.join(","))
+            },
+            RispExp::Func(_) => "Function {}".to_string(),
+            RispExp::Lambda(_) => "Lambda {}".to_string()
+        };
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Debug)]
+enum RispErr {
+    Reason(String),
+}
+
+#[derive(Clone)]
+struct RispEnv<'a> {
+    data: HashMap<String, RispExp>,
+    outer: Option<&'a RispEnv<'a>>,
+}
+
+fn tokenize(expr: String) -> Vec<String> {
+    expr
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(|x| x.to_string())
+        .collect()
+}
+
+fn parse<'a>(tokens: &'a [String]) -> Result<(RispExp, &'a [String]), RispErr> {
+    let (token, rest) = tokens.split_first()
+        .ok_or(
+            RispErr::Reason("could not get token".to_string())
+        )?;
+    match &token[..] {
+        "(" => read_seq(rest),
+        ")" => Err(RispErr::Reason("unexpected `)`".to_string())),
+        _ => Ok((parse_atom(token), rest)),
+    }
+}
+
+fn parse_atom(token: &str) -> RispExp {
+    match token.as_ref() {
+        "true" => RispExp::Bool(true),
+        "false" => RispExp::Bool(false),
+        _ => {
+            let potential_float: Result<f64, ParseFloatError> = token.parse();
+            match potential_float {
+                Ok(v) => RispExp::Number(v),
+                Err(_) => RispExp::Symbol(token.to_string().clone())
+            }
+        }
+    }
+}
+
+fn read_seq<'a>(tokens: &'a [String]) -> Result<(RispExp, &'a [String]), RispErr> {
+    let mut res: Vec<RispExp> = vec![];
+    let mut xs = tokens;
+    loop {
+        let (next_token, rest) = xs
+            .split_first()
+            .ok_or(RispErr::Reason("could not find closing `)`".to_string()))
+            ?;
+        if next_token == ")" {
+            return Ok((RispExp::List(res), rest))
+        }
+        let (exp, new_xs) = parse(&xs)?;
+        res.push(exp);
+        xs = new_xs;
+    }
+}
+
 fn main() {
     emola_rs::emola::reader::read();
 }
